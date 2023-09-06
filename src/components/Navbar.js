342e858efb03e03faa20828da38dac98864fc345
Nicole Yang <nicoleyry@outlook.com>
2023-09-06 17:10:59 +0800

    add mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
 import '../styles/navbar.scss';
 import Logo from '../assets/images/logo.svg';
 import Burger from '../assets/images/icon-hamburger.svg';
+import Close from '../assets/images/icon-close.svg';
 import Button from './Button';
 
-export default function navbar() {
-	let openMenu = () => {
-		console.log('Open Menu');
+export default function navbar({ isOpened, setIsOpened }) {
+	let openMenuHandler = () => {
+		setIsOpened(true);
+		isOpened ? setIsOpened(false) : setIsOpened(true);
+	};
+	let closeMenuHandler = (e) => {
+		let classlist = e.target.classList;
+		if (!classlist.contains('burger')) {
+			classlist.contains('menu-container') || classlist.contains('nav-menu')
+				? setIsOpened(true)
+				: setIsOpened(false);
+		}
 	};
 
 	return (
-		<div className='Navbar'>
+		<div className='Navbar' onClick={closeMenuHandler}>
 			<div className='nav-container'>
 				<div className='nav-left'>
 					<img className='logo' src={Logo} alt='logo' />
@@ -23,7 +33,21 @@ export default function navbar() {
 				</div>
 				<div className='nav-right'>
 					<Button orangeBtn={true} text='Get Started' />
-					<img className='burger' onClick={openMenu} src={Burger} alt='burger-menu' />
+					<img
+						className='burger'
+						onClick={openMenuHandler}
+						src={`${isOpened ? Close : Burger}`}
+						alt='burger-menu'
+					/>
+				</div>
+			</div>
+			<div className={`menu-cover ${isOpened ? 'show' : ''}`} onClick={closeMenuHandler}>
+				<div className='menu-container'>
+					<p className='nav-menu'>Pricing</p>
+					<p className='nav-menu'>Product</p>
+					<p className='nav-menu'>About Us</p>
+					<p className='nav-menu'>Careers</p>
+					<p className='nav-menu'>Community</p>
 				</div>
 			</div>
 		</div>
--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -2,11 +2,14 @@ import '../styles/topsection.scss';
 import Navbar from './Navbar';
 import IntroDiagram from '../assets/images/illustration-intro.svg';
 import Button from './Button';
+import { useState } from 'react';
 
 export default function TopSection() {
+	const [isOpened, setIsOpened] = useState(false);
+
 	return (
 		<div className='TopSection'>
-			<Navbar />
+			<Navbar isOpened={isOpened} setIsOpened={setIsOpened} />
 			<div className='intro'>
 				<div className='content'>
 					<h1 className='title'>Bring everyone together to build better products.</h1>
